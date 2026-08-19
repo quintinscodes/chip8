e52cb41fbfc2708c3291bea92686d50be7c93319
cvarela3344 <claudio.varela@usm.cl>
2026-08-19 18:57:26 -0400

    plataforma y main hechos, solo corre logo ibm :P

--- chip8.h
+++ chip8.h
@@ -7,7 +7,7 @@
 
 const unsigned int FONTSET_SIZE = 80;
 
-uint8_t fontset[FONTSET_SIZE] =
+inline uint8_t fontset[FONTSET_SIZE] =
 {
 	0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
 	0x20, 0x60, 0x20, 0x20, 0x70, // 1
